ef4e9cf3d8d0c4c192c8f4b685ef94541ab73a44
Huriel Lopes <huriellopes1996@gmail.com>
2020-04-02 20:03:26 -0300

    Ajuste no config/db

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
   user: 'postgres',
-  password: 299610,
+  password: 'suasenha',
   host: 'localhost',
   port: 5432,
   database: 'gymmanager'
